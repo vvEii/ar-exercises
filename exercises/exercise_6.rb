aa4fb1e49672c5efc8edb014f75ada8e81fd97f6
vvEii <coopershang125@gmail.com>
2020-11-11 15:46:52 -0800

    Create one-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id:4)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani",
+  hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Virani",
+  hourly_rate: 90)
+@store2.employees.create(first_name: "Mike", last_name: "Virani",
+  hourly_rate: 100)
+@store2.employees.create(first_name: "Tom", last_name: "Virani",
+  hourly_rate: 40)
+@store3.employees.create(first_name: "Jack", last_name: "Virani",
+  hourly_rate: 30)
+@store3.employees.create(first_name: "Carter", last_name: "Virani",
+  hourly_rate: 60)
